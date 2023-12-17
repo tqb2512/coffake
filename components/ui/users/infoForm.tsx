1138fa2978795814edebf85e2d393ac4fe0d0102
Mingu <vhnm3004@outlook.com>
2023-12-17 15:12:25 +0700

    Added infoForm for Employee's UI

--- components/ui/users/infoForm.tsx
+++ components/ui/users/infoForm.tsx
@@ -1,26 +1,72 @@
-'use client'
+"use client";
 
-import { Employee } from '@prisma/client';
-import React from "react"
+import {
+  Divider,
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@nextui-org/react";
+import { Employee } from "@prisma/client";
+import React from "react";
 
-export default function UserInfoForm({ params }: { params: { username: string } }) {
-    
-    const [employee, setEmployee] = React.useState({} as Employee);
+const columns = [
+  { name: "Name", key: "name" },
+  { name: "Email", key: "email" },
+  { name: "Position", key: "position" },
+  { name: "Salary", key: "salary" },
+  { name: "Username", key: "username" },
+  { name: "Phone Number", key: "phone" },
+];
 
-    React.useEffect(() => {
-        fetch(`/api/users/${params.username}`)
-            .then((res) => res.json())
-            .then((data) => setEmployee(data));
-    }, [params.username])
-    
-    return (
-        <div>
-            <h1>{employee.name}</h1>
-            <p>{employee.email}</p>
-            <p>{employee.position}</p>
-            <p>{employee.salary}</p>
-            <p>{employee.username}</p>
-            <p>{employee.phone}</p>
-        </div>
-    )
+export default function UserInfoForm({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const [employee, setEmployee] = React.useState({} as Employee);
+
+  React.useEffect(() => {
+    fetch(`/api/users/${params.username}`)
+      .then((res) => res.json())
+      .then((data) => setEmployee(data));
+  }, [params.username]);
+
+  return (
+    <div>
+      <label className="font-light text-violet-800 text-3xl">
+        {"Employee's Details"}
+      </label>
+      <Divider className="my-4" />
+      <Table>
+        <TableHeader>
+          {columns.map((column) => (
+            <TableColumn className="font-bold" key={column.key}>
+              {column.name}
+            </TableColumn>
+          ))}
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>{employee.name}</TableCell>
+            <TableCell>{employee.email}</TableCell>
+            <TableCell>{employee.position}</TableCell>
+            <TableCell>{employee.salary}</TableCell>
+            <TableCell>{employee.username}</TableCell>
+            <TableCell>{employee.phone}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+    // <div>
+    //     <h1>{employee.name}</h1>
+    //     <p>{employee.email}</p>
+    //     <p>{employee.position}</p>
+    //     <p>{employee.salary}</p>
+    //     <p>{employee.username}</p>
+    //     <p>{employee.phone}</p>
+    // </div>
+  );
 }
